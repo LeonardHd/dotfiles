f8ebd5c6ef5cf2ef5bd9c62fa523741d9aeaf89a
Leonard Herold <92177433+LeonardHd@users.noreply.github.com>
2025-03-15 09:48:20 +0000

    tests: add dockerfile-based test

--- tests/run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# cd ~ && sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply --data=false LeonardHd
-
-# sh -c "$(curl -fsLS get.chezmoi.io)" -- init --data=false --apply=true
--- tests/test.sh
+++ tests/test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build -t chezmoi-test -f tests/Dockerfile.ubuntu .
+docker build -t chezmoi-test --progress plain -f tests/Dockerfile.ubuntu .
 docker run chezmoi-test
 
